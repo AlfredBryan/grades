194a77ec4b9ace4d9d79a8a9cc870a001d4a93c4
alfredbryan <alfred.bryan@gmail.com>
2018-01-31 10:29:52 +0100

    Showing Grade

---
+++ grade.js
@@ -0,0 +1,23 @@
+var students = [['Francis', 80], ['Amarachi', 78], ['Nkechi', 84], ['Chioma', 98], ['Bryan', 67]];
+var Avgmarks = 0;
+
+for (var i=0; i < students.length; i++){
+	Avgmarks += students[i][1];
+	var avg = (Avgmarks/students.length);
+}
+alert("Average grade: " + (Avgmarks)/students.length);
+     if (avg < 60){
+     	console.log("Grade : F");
+     }
+     else if (avg < 70){
+     	console.log("Grade : D");
+     }
+     else if (avg < 80){
+     	console.log("Grade : C");
+     }
+     else if (avg < 90){
+     	console.log("Grade : B");
+     }
+     else if (avg < 100){
+     	console.log("Grade : A");
+     }
